f20e6859935fb9edf50723df0e342c5a7c140100
mark-sec <mark.sec.code@gmail.com>
2025-01-27 07:12:07 -0600

    refactor

--- src/components/WaitlistForm.tsx
+++ src/components/WaitlistForm.tsx
@@ -1,25 +1,5 @@
 import Helmet from "react-helmet";
 
-// export default function WaitlistWidget() {
-//     return (
-//         <>
-//             <div
-//                 id="getWaitlistContainer"
-//                 data-waitlist_id="23473"
-//                  data-widget_type="WIDGET_3"
-//             ></div>
-//             <Helmet>
-//                 <link
-//                 rel="stylesheet"
-//                 type="text/css"
-//                 href="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.css"
-//                 />
-//                 <script src="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.js"></script>
-//             </Helmet>
-//         </>
-//     );
-// }
-
 
 export default function WaitlistWidget() {
     return (
