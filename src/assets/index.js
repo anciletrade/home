86c91ff29955383fad489b44cf956096e51a8dca
mark-sec <mark.sec.code@gmail.com>
2025-01-27 06:55:42 -0600

    change logo

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,11 +1,11 @@
 import logo from "./logo.svg";
-import logoBlack from "./ancile_dark1.png";
+import logoBlack from "./ancile_dark.png";
 import logoWhite from "./ancile_white.png";
-import ancile_home from "./ancile_home.png";
+import ancile_home from "./ancile_home_dark.png";
 import service_one from "./service_one.png";
 import service_two from "./service_two.png";
 import service_three from "./service_three.png";
 import integrations from "./integrations.svg";
-import ancile_home_light from "./ancile_light.png";
+import ancile_home_light from "./ancile_home_light.png";
 
 export {logoWhite,logoBlack, logo,ancile_home,service_one,service_two,service_three,integrations, ancile_home_light}
