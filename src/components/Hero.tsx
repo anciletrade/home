32d90146cc04422db95132069d007ccb0d314dd4
Zoe Serano <edwardserano@gmail.com>
2025-02-04 14:06:42 +0100

    removed exclamation mark

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -129,7 +129,7 @@ export default function Hero() {
               })}
             >
               {" "}
-              Be Protected!
+              Be Protected
             </Typography>
           </Typography>
           <Typography
